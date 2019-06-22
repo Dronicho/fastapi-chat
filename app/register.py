21dfc777b3c72b75173fcdfeee1b618a583bfc3a
Andrey <takemyenergy55@gmail.com>
2019-06-22 16:35:55 +0500

    Register

---
+++ app/register.py
@@ -0,0 +1,23 @@
+from typing import List
+
+from app import app
+from app.db import database, users
+from app.models import User, UserInDB
+from app.authenticate import get_password_hash
+
+
+@app.post('/register', response_model=User)
+async def create_user(user: User):
+    hash = get_password_hash(user.password)
+    print('hashed password:', hash)
+    q = users.insert().values(email=user.email, username=user.username, hashed_password=hash)
+    last_record_id = await database.execute(q)
+    return {**user.dict(), 'id': last_record_id}
+
+
+@app.get('/register', response_model=List[UserInDB])
+async def get_users():
+    q = users.select()
+    return await database.fetch_all(q)
+
+
